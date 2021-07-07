ac1d041b2e905cd6fc84ccc72c3aac728cbce103
Américo MQ <americo.meneses.contractor@bbva.com>
2021-07-07 17:37:30 -0500

    setting vieew programatically

---
+++ ToDoListGenerics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListGenerics
+//
+//  Created by IDS Comercial on 07/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
